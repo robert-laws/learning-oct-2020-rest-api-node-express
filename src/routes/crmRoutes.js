afdea664be8e7ad4f6034e958a8dd2614ce7e828
Robert Laws <roblaws@me.com>
2020-10-04 15:24:43 +0300

    add get and post endpoints

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,18 +1,15 @@
+import { getContacts, addNewContact } from '../controllers/crmController';
+
 const routes = (app) => {
   app
     .route('/contact')
-    .get(
-      (req, res, next) => {
-        // middleware
-        console.log(`Request from: ${req.originalUrl}`);
-        console.log(`Request type: ${req.method}`);
-        next();
-      },
-      (req, res, next) => {
-        res.send('GET request successful!');
-      }
-    )
-    .post((req, res) => res.send('POST request successful!'));
+    .get((req, res, next) => {
+      // middleware
+      console.log(`Request from: ${req.originalUrl}`);
+      console.log(`Request type: ${req.method}`);
+      next();
+    }, getContacts)
+    .post(addNewContact);
 
   app
     .route('/contact/:contactID')
