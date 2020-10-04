e4a6214b11d95288ac5abd77b55b3e642bc0c443
Robert Laws <roblaws@me.com>
2020-10-04 20:09:25 +0300

    add getContactById controller action

--- src/controllers/crmController.js
+++ src/controllers/crmController.js
@@ -22,3 +22,12 @@ export const getContacts = (req, res) => {
     res.json(contacts);
   });
 };
+
+export const getContactById = (req, res) => {
+  Contact.findById(req.params.contactID, (err, contact) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json(contact);
+  });
+};
--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,4 +1,8 @@
-import { getContacts, addNewContact } from '../controllers/crmController';
+import {
+  getContacts,
+  getContactById,
+  addNewContact,
+} from '../controllers/crmController';
 
 const routes = (app) => {
   app
@@ -13,6 +17,7 @@ const routes = (app) => {
 
   app
     .route('/contact/:contactID')
+    .get(getContactById)
     .put((req, res) => res.send('PUT request successful!'))
     .delete((req, res) => res.send('DELETE request successful!'));
 };
