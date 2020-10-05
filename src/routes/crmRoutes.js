6294e97a7284ad8077e5386e6c5b14845652ff19
Robert Laws <roblaws@me.com>
2020-10-05 09:54:49 +0300

    add DELETE endpoint

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -3,6 +3,7 @@ import {
   getContactById,
   addNewContact,
   updateContact,
+  deleteContact,
 } from '../controllers/crmController';
 
 const routes = (app) => {
@@ -14,13 +15,13 @@ const routes = (app) => {
       console.log(`Request type: ${req.method}`);
       next();
     }, getContacts)
-    .post(addNewContact);
+    .post(addNewContact); //post endpoint
 
   app
     .route('/contact/:contactID')
-    .get(getContactById)
-    .put(updateContact)
-    .delete((req, res) => res.send('DELETE request successful!'));
+    .get(getContactById) // get a specific contact
+    .put(updateContact) // updating a specific contact
+    .delete(deleteContact); // deleting a specific contact
 };
 
 export default routes;
